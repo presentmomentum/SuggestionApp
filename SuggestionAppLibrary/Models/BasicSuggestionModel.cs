5f8e98eedc646edeeabe4bdc64e9b80470cf4829
Yordan Bratanov <jordan.bratanov@yahoo.com>
2022-04-16 22:58:45 +0300

    Added Data Models

---
+++ SuggestionAppLibrary/Models/BasicSuggestionModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SuggestionAppLibrary.Models;
+public class BasicSuggestionModel
+{
+   public BasicSuggestionModel()
+   {
+
+   }
+
+   public BasicSuggestionModel(SuggestionModel suggestion)
+   {
+      Id = suggestion.Id;
+      Suggestion = suggestion.Suggestion;
+   }
+
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string Id { get; set; }
+   public string Suggestion { get; set; }
+}
---
+++ SuggestionAppLibrary/Models/BasicUserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace SuggestionAppLibrary.Models;
+public class BasicUserModel
+{
+   public BasicUserModel()
+   {
+
+   }
+
+   public BasicUserModel(UserModel user)
+   {
+      Id = user.Id;
+      DisplayName = user.DisplayName;
+   }
+
+   [BsonRepresentation(BsonType.ObjectId)]
+   public string Id { get; set; }
+   public string DisplayName { get; set; }
+}
